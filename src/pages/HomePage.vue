905a28a82429dda3d1de7abde5633fce757f4289
Brad Lewis <BLewis739@gmail.com>
2022-05-24 15:09:21 -0400

    More text on homepage, added brand statement

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -9,8 +9,18 @@
       </div>
     </header>
     <div class="additional-text">
+      <div class="IAMA">I am a...</div>
+      <div class="row1">
       <div class="job-title">Software Engineer</div>
       <div class="job-title">Full Stack Developer</div>
+      </div><div class="row2">
+      <div class="job-title">Frontend Developer</div>
+      <div class="job-title">Backend Developer</div>
+      </div>
+      <div class="brand-statement">
+        I am a full stack developer living in the Greater Boston area. I am a highly motivated problem solver who thrives in work environments that require creativity, flexibility, collaboration, and unique challenges. 
+        Outside of programming, I have several artistic pursuits in design, multimedia production, and live performance. Prior to becoming a developer, I worked as a science teacher, and helped young people pique their interest in technology.
+      </div>
     </div>
   </div>
 </template>
