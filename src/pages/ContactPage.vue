1eded52d7197a768ac136fca7f92226aef7ee822
Brad Lewis <BLewis739@gmail.com>
2022-05-26 12:56:22 -0400

    Contact page links and style changes

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -3,11 +3,22 @@
     <header class="project-page-header">
       Contact Me
     </header>
-    <div className="social-links">
-          <h3>External Links</h3>
-          
-          <a href="https://www.linkedin.com/in/bradlewis739/" class="social-link">Brad's LinkedIn</a>
-          <a href="https://github.com/BLewis739" class="social-link">Brad's GitHub</a>
+    <div class="contact-text-container">
+      <div class="intro-contact-text">
+        My preferred method of contact is email, which is posted below. There are also 
+        links to my LinkedIn and GitHub profiles. Please feel free to reach out anytime!
+      </div>
+      <div class="social-links">
+        <div class="email">
+          <a href="mailto:blewis739@gmail.com" ><img src="../assets/EmailIcon.png" class="icon"></a> BLewis739@gmail.com
+        </div>
+        <div class="linkedin">
+          <a href="https://www.linkedin.com/in/bradlewis739/" target="_blank"><img src="../assets/LinkedInIcon.png" class="icon"></a> linkedin.com/in/bradlewis739
+        </div>
+        <div class="github">
+          <a href="https://github.com/BLewis739" target="_blank"><img src="../assets/GithubIcon.png" class="icon"></a> github.com/BLewis739
+        </div>
+      </div>
     </div>
   </div>
 </template>
